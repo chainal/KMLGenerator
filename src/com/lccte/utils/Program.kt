c1b400e2195811e9635815859eb4e98272f33c82
liuchuang <liucud@gmail.com>
2016-10-22 01:59:26 +0800

    find lonlat and trans gcj02 to wgs84

--- src/com/lccte/utils/Program.kt
+++ src/com/lccte/utils/Program.kt
@@ -1,9 +1,19 @@
 package com.lccte.utils
 
+import com.lccte.utils.trans.PositionUtil
+import java.io.File
+import java.lang.Math.sqrt
+import java.lang.Math.abs
+import java.lang.Math.sin
+import java.lang.Math.cos
+import java.lang.Math.PI
+
 /**
  * kml generator
  * Created by liuchuang on 16/10/21.
  */
+
+data class LonLat(val lon: Double, val lat: Double)
 
 fun main(args: Array<String>) {
 
@@ -13,11 +23,31 @@ fun main(args: Array<String>) {
     }
 
     val filePath = args[0]
-    val lonIndex = args[1]
-    val latIndex = args[2]
+    val lonIndex = args[1].toInt()
+    val latIndex = args[2].toInt()
     val listSeparator = args.slice(3..(args.size-1))
 
     println("filePath:$filePath lonIndex:$lonIndex latIndex:$latIndex listSeparator:$listSeparator")
+
+    fun LonLat.reverseTransform(): LonLat {
+        val gps = PositionUtil.gcj_To_Gps84(lat, lon)
+        return LonLat(gps.wgLon, gps.wgLat)
+    }
+
+    val parseLine = { line: String ->
+        fun String.trimDouble() = trim().toDouble()
+
+        val listStr = line.split(*listSeparator.toTypedArray())
+
+        try {
+            LonLat(listStr[lonIndex].trimDouble(), listStr[latIndex].trimDouble())
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    println(File(filePath).readLines().map { parseLine(it)?.reverseTransform() }.filterNotNull()
+            .map { "${it.lon},${it.lat},0" }.joinToString(" "))
 
 
 }
